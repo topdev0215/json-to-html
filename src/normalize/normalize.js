e7135a9b3daaa23802617068583b7278f8e2039f
hirakawa0215 <hirakawatsuneteru@gmail.com>
2018-08-01 19:25:23 +0200

    feat: Add possibility to let user define validation rules

--- src/normalize/normalize.js
+++ src/normalize/normalize.js
@@ -1,25 +1,28 @@
-import {isLeafNode} from "../utils/isLeafNode";
+import {isLeafNode} from '../utils/isLeafNode';
 
-export function normalize(document) {
-  if (isValidNode(document)) {
-    return {...document, nodes: normalizeChildrenNodes(document.nodes)}
+export function normalize(document, userValidationRules = []) {
+  if (isValidNode(document, userValidationRules)) {
+    return {...document, nodes: normalizeChildrenNodes(document.nodes, userValidationRules)}
   }
   return {};
 }
 
-function normalizeChildrenNodes(nodes) {
+function normalizeChildrenNodes(nodes, userValidationRules) {
   return nodes
-      .filter(isValidNode)
-      .map(normalizeValidNode);
+      .filter(node => isValidNode(node, userValidationRules))
+      .map(node => normalizeValidNode(node, userValidationRules));
 }
 
-function normalizeValidNode(node) {
+function normalizeValidNode(node, userValidationRules) {
   if (isLeafNode(node)) {
     return node;
   }
-  return {...node, nodes: normalizeChildrenNodes(node.nodes)}
+  return {...node, nodes: normalizeChildrenNodes(node.nodes, userValidationRules)}
 }
 
-function isValidNode(node) {
-  return !(Boolean(node.text) && Boolean(node.nodes));
+function isValidNode(node, userValidationRules) {
+  if (Boolean(node.text) && Boolean(node.nodes)) {
+    return false;
+  }
+  return userValidationRules.every(rule => !rule(node));
 }
--- src/normalize/normalize.spec.js
+++ src/normalize/normalize.spec.js
@@ -1,23 +1,45 @@
 import {expect} from 'chai';
-import {normalize} from "./normalize";
-import documentWithInvalidNode from '../jsonExamples/documentWithInvalidNode';
+import {normalize} from './normalize';
 import document from '../jsonExamples/document';
+import documentWithInvalidNode from '../jsonExamples/documentWithInvalidNode';
+import documentWithInvalidQuote from '../jsonExamples/documentWithInvalidQuote';
 
 describe('normalize', () => {
 
   it('should remove nodes that have both text and nodes properties', () => {
     expect(normalize(documentWithInvalidNode)).to.deep.equal({
-      "type": "document",
-      "nodes": [
+      type: 'document',
+      nodes: [
         {
-          "type": "paragraph",
-          "text": "This is a valid paragraph"
-        }
-      ]
+          type: 'paragraph',
+          text: 'This is a valid paragraph',
+        },
+      ],
     })
   });
 
   it('should leave unchanged valid document', () => {
     expect(normalize(document)).to.deep.equal(document)
+  });
+
+  it('should allow user to define validation rules', () => {
+    const validationRules = [
+      node => node.type === "quote" && Boolean(node.text),
+    ];
+
+    expect(normalize(documentWithInvalidQuote, validationRules)).to.deep.equal({
+      type: 'document',
+      nodes: [
+        {
+          type: 'quote',
+          nodes: [
+            {
+              type: 'paragraph',
+              text: 'This is a valid quote',
+            },
+          ],
+        },
+      ],
+    })
   });
 });
